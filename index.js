4808541188e4910341391212c958a7315502afea
Chris M <chris.j.major@gmail.com>
2016-02-24 20:29:18 +0000

    Database storage

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var pg = require('pg');
 var unique = require('unique-wallpaper')({});
 var app = express();
 
@@ -10,6 +11,50 @@ app.get('/gen/:id(\\d+)', function (req, res) {
   res.setHeader('Content-Type', 'image/svg+xml');
   res.send(unique.start(req.params.id).writeXML( true /* pretty */ ));
 });
+
+var DEFAULT_DB = "postgres://localhost:5432/results";
+
+app.post('/vote', function (request, response) {
+  pg.connect(process.env.DATABASE_URL||DEFAULT_DB, function(err, client, done) {
+    if ( err )
+     { console.error(err); return response.send("Error " + err); }
+    var best = request.query.best;
+    var bad1 = request.query.bad1;
+    var bad2 = request.query.bad2;
+    var ip = request.connection.remoteAddress;
+    client.query('INSERT INTO results(best,bad1,bad2,ip) VALUES ($1,$2,$3,$4)', [best,bad1,bad2,ip] , function(err) {
+      done();
+      if (err){
+        console.error(err);
+        response.send("Error " + err);
+      }else{
+        response.send("OK");
+      }
+    });
+  });
+});
+
+
+app.get('/data', function (request, response) {
+  pg.connect(process.env.DATABASE_URL||DEFAULT_DB, function(err, client, done) {
+    if ( err )
+     { console.error(err); return response.send("Error " + err); }
+    client.query('SELECT * FROM results', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       {
+         var str = "best,bad,bad,ip,timestamp\n";
+         for ( var rowid in result.rows ){
+           var r = result.rows[rowid];
+           str+= r.best+","+r.bad1+","+r.bad2+","+r.ip+","+r.my_timestamp+"\n";
+         }
+         response.attachment('data.csv').send(str);
+       }
+    });
+  });
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
--- public/app.js
+++ public/app.js
@@ -1,18 +1,37 @@
 $(function(){
+  var activeSet = [];
+
   pickRandoms();
 
   function pickRandoms(){
+    activeSet = [];
     $( ".sample" ).each(function( index , e ) {
       var element = $(e);
       var seed = Math.floor(Math.random() * 4028);
       element.data("seed",seed);
       element.find("button").text("vote "+seed);
       element.find("img").attr("src","/gen/"+seed);
+      activeSet.push(seed);
     });
   }
 
-  $( "body" ).on( "click", "button", function() {
-    pickRandoms();
+  $( "body" ).on( "click", "button", function(event) {
+    var target = $(event.target);
+    var sample = target.closest(".sample");
+    if ( sample ){
+      var best = sample.data("seed");
+      var n = activeSet.indexOf(best);
+      activeSet.splice( n , 1 );
+      var others = activeSet;
+      $.post( "vote?best="+best+"&bad1="+activeSet[0]+"&bad2="+activeSet[1], function( data ) {
+        // ignore
+        console.log(data);
+      });
+      pickRandoms();
+    }else{
+      // reload button
+      pickRandoms();
+    }
   });
 
 });
